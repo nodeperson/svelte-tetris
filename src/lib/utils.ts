0689a1cdb02de193eb18f369bbfb9a769e6e8711
nodeperson <nodeperson@gmail.com>
2022-07-27 11:39:05 +0300

    make helpers in utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import type { FigureView } from "./entities";
+import { figureWidth } from "./figures";
+import { BoxWidth } from "./settings";
+
+export function initialColumn(figure: FigureView) {
+    return Math.floor((BoxWidth - figureWidth(figure)) / 2);
+}
+
+export function deepClone<T>(any:T) {
+    return <T>JSON.parse(JSON.stringify(any));
+}
