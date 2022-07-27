485429ed9fc6be0e2f16a6590482d4ef339f6af1
nodeperson <nodeperson@gmail.com>
2022-07-27 12:12:37 +0300

    test Controller in the console

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+  import Controller from "./components/Controller.svelte";
+
+  function handleControllerEvent(eventName: string) {
+    console.log(eventName)
+  }
 </script>
 
 <main>
-
+  <Controller
+    on:left={() => handleControllerEvent('left')}
+    on:right={() => handleControllerEvent('right')}
+    on:down={() => handleControllerEvent('down')}
+    on:drop={() => handleControllerEvent('drop')}
+    on:turn={() => handleControllerEvent('turn')}
+    on:pause={() => handleControllerEvent('pause')}
+  />
 </main>
 
 <style>
-
 </style>
