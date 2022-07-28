cf372bfea5aa4cde556e69ebdb52ab0eeab8b240
nodeperson <nodeperson@gmail.com>
2022-07-28 08:15:46 +0300

    create Tetris={...} in src/game.ts

---
+++ src/game.ts
@@ -0,0 +1,15 @@
+function pause() { }
+function moveLeft() { }
+function moveRight() { }
+function moveDown() { }
+function drop() { }
+function turn() { }
+
+export const Tetris = {
+    pause,
+    moveLeft,
+    moveRight,
+    moveDown,
+    drop,
+    turn
+}
